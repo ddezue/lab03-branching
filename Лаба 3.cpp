23bcc6be483852fb88e357ba3db55e60866f9d9f
ddezue <LYANAZAYKA@MAIL.RU>
2025-10-03 08:36:13 +0700

    Update 2.0 Лаба 3.cpp

--- Лаба 3.cpp
+++ Лаба 3.cpp
@@ -2,7 +2,7 @@
  * Автор: Бакланова Ляна *
  * Вариант: 1            *
  *************************/
- 
+
 #include <iostream>
 #include <cmath>
 #include <iomanip>
@@ -13,29 +13,31 @@ int main() {
   int s1 = 60;
   double diameter = 25.4;
   double Re = 2 * pow(10, 4);
-  int iteration;
+  int loopIteration;
   double Nu;
   double delta;
   double s2;
   double sg;
-    
-  cout << fixed << setprecision(2);
-    
-  for (iteration = 0; iteration < 5; ++iteration) { 
-    cout << "S2=";
+  int numOfDigitsAfterTheDecimalPoint = 2;
+  int numOfIterations = 5;
+  
+  cout << fixed << setprecision(numOfDigitsAfterTheDecimalPoint);
+
+  for (loopIteration = 0; loopIteration < numOfIterations; ++loopIteration) { 
+    cout << "S2 =";
     cin >> s2;
-        
-    sg = diameter * sqrt(0.25 * pow(s1 / diameter, 2) + pow(s2 / diameter, 2));
+
+    sg = diameter * sqrt(0.25 * pow(s1 / diameter, 2.0) + pow(s2 / diameter, 2.0));
     delta = (s1 - diameter) / (sg - diameter);
-        
+
     if (delta < 0.7) {
       Nu = 0.27 * pow(Re, 0.6) * delta;
     } else {
-    Nu = 0.295 * pow(Re, 0.6) * pow(delta, 0.25);
+      Nu = 0.295 * pow(Re, 0.6) * pow(delta, 0.25);
     }
 
-  cout << "Nu = " << Nu << "\t Delta = " << delta << endl;
+    cout << "Nu = " << Nu << "\t Delta = " << delta << endl;
   }
-    
+
   return 0;
 }
